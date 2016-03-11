06f9dcb25ddb2985e874b7c9765965b67a4f0302
mthies1 <maureen.e.thiessen@gmail.com>
2016-03-11 13:31:01 -0600

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -11,5 +11,6 @@ function initMap() {
  // map.data.loadGeoJson('gstore.unm.edu/apps/rgis/datasets/d93688b6-eece-4253-a6e8-5b18c8834bef/S_GEN_STRUCT.derived.json');
  map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/61d841ee66bf4c1a862777607dc6e91c_7.geojson');
  map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/82b9ca8a57e44eccb0d104ff7d325829_0.geojson');
+ map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/357e79f8af3b4bb1a3ada6271507d6c3_0.geojson');
  }
 //
