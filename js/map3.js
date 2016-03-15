07e98be58d036306f100b04d697cbecba852a98a
mthies1 <maureen.e.thiessen@gmail.com>
2016-03-15 09:25:40 -0500

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -6,7 +6,7 @@ function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
    center: {lat: 35.0, lng: -78.64},
    zoom: 11,
-   zoomControl: true
+   //zoomControl: true
  });
  // map.data.loadGeoJson('http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=la&parameterCd=00060,00065&siteType=ST');
  // map.data.loadGeoJson('gstore.unm.edu/apps/rgis/datasets/d93688b6-eece-4253-a6e8-5b18c8834bef/S_GEN_STRUCT.derived.json');
