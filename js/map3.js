8e63c5dfef261df1d43ccff779ff9c3d73cdd25a
mthies1 <maureen.e.thiessen@gmail.com>
2016-03-15 09:30:07 -0500

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -4,7 +4,7 @@
 var map;
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
-   center: {lat: 35.0, lng: -78.64},
+   center: {lat: 34.7, lng: -78.64},
    zoom: 11
    //zoomControl: true
  });
