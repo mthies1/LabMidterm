ae3e2325b4c1067d61c8e800194695ca6f1c56dc
mthies1 <maureen.e.thiessen@gmail.com>
2016-03-15 09:54:13 -0500

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -8,17 +8,12 @@ function initMap() {
    center: {lat: 33.5, lng: -78.64}
    //zoomControl: true
  });
- // map.data.loadGeoJson('http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=la&parameterCd=00060,00065&siteType=ST');
- // map.data.loadGeoJson('gstore.unm.edu/apps/rgis/datasets/d93688b6-eece-4253-a6e8-5b18c8834bef/S_GEN_STRUCT.derived.json');
- //map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/61d841ee66bf4c1a862777607dc6e91c_7.geojson');
- //map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/82b9ca8a57e44eccb0d104ff7d325829_0.geojson');
- //map.data.loadGeoJson('http://clt.charlotte.opendata.arcgis.com/datasets/357e79f8af3b4bb1a3ada6271507d6c3_0.geojson');
- map.data.loadGeoJson('http://data.wake.opendata.arcgis.com/datasets/e971bec1f1d94c9d927548b6d45455d4_0.geojson');
- //map.data.loadGeoJson('http://data.wake.opendata.arcgis.com/datasets/e10e1fafe835491db6d68b5ececa7d66_0.geojson');
+ 
  var ctaLayer = new google.maps.KmlLayer({
    url: 'http://waterservices.usgs.gov/nwis/site/?format=ge,1.0&countyCd=37183&siteOutput=expanded&seriesCatalogOutput=true&outputDataTypeCd=iv,dv,qw&siteType=LK,ST,WE',
    map: map
  });
-
+ 
+map.data.loadGeoJson('http://data.wake.opendata.arcgis.com/datasets/e971bec1f1d94c9d927548b6d45455d4_0.geojson');
  
 }
