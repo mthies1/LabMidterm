07042a5682a8d822ec1883dfd4e6815fab3d1455
mthies1 <maureen.e.thiessen@gmail.com>
2016-03-15 22:37:24 -0500

    Update map3.js

--- js/map3.js
+++ js/map3.js
@@ -1,13 +1,4 @@
 
-
-function addmypopup(feature, layer)
-	{
-		msg = Feature.properties.PROPWS + "<br>" + "<br>" + 
-		"STATUS: " + Feature.properties.CRIT_NON + "<br>" + 
-		"MORE INFO: " + '<a href="http://data.wake.opendata.arcgis.com/datasets/e971bec1f1d94c9d927548b6d45455d4_0"></a>';
-		layer.bindPopup(msg);
-	}
-
 var map;
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
@@ -17,6 +8,23 @@ function initMap() {
  
  map.data.loadGeoJson('http://data.wake.opendata.arcgis.com/datasets/e971bec1f1d94c9d927548b6d45455d4_0.geojson');
  
+ // Create an infowindow object to use later
+var infowindow = new google.maps.InfoWindow();
+
+/* Create a "listener" that will wait for the user to click an earthquake point,
+ * and then display the infowindow with details about that earthquake.
+ */
+map.data.addListener('click', function(event) {
+  // in the geojson feature that was clicked, get the "place" and "mag" attributes
+  var shed = event.feature.getProperty("PROPWS");
+  var status = event.feature.getProperty("NON_CRIT");
+  var html = shed + ' watershed, status: ' + status; 
+  infowindow.setContent(html); // show the html variable in the infowindow
+  infowindow.setPosition(event.feature.getGeometry().get()); // anchor the infowindow at the marker
+  infowindow.setOptions({pixelOffset: new google.maps.Size(0,-30)}); // move the infowindow up slightly to the top of the marker icon
+  infowindow.open(map);
+});
+ 
  var ctaLayer = new google.maps.KmlLayer({
    url: 'http://waterservices.usgs.gov/nwis/site/?format=ge,1.0&countyCd=37183&siteOutput=expanded&seriesCatalogOutput=true&outputDataTypeCd=iv,dv,qw&siteType=LK,ST,WE',
    preserveViewport: true,
@@ -25,4 +33,4 @@ function initMap() {
  
 }
 
-L.geoJson(geojsonFeature, {onEachFeature: addmypopup}).addTo(map);
+
